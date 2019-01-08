13ba9ac45460885ba30594ae3b063894316da944
Aleem Isiaka <aleemisiaka@gmail.com>
2019-01-08 23:29:25 +0100

    Completes the library

---
+++ src/defaults.js
@@ -0,0 +1,4 @@
+export default {
+  VERSION: "v3/3.0", // Version of the script to load
+  interactive: true // Loads interactivity script
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Yes");
+import getJS from "get-js";
+import defaults from "./defaults";
+import merge from "merge";
+``;
+
+const buildScriptURLs = (version = defaults.VERSION) => [
+  `http://js.api.here.com/${version}/mapsjs-service.js`, // Service
+  `http://js.api.here.com/${version}/mapsjs-mapevents.js` // Events
+];
+
+const merger = options => merge(defaults, options);
+
+const scriptLoader = options => {
+  const _options = merger(options || {});
+  const { VERSION, version, interactive } = _options;
+
+  const _v = version || VERSION;
+  const urls = buildScriptURLs(_v);
+
+  // First let us remove the events if it is not needed. PERFORMANCE!!!
+  !interactive ? urls.splice(2, 1) : null;
+  const coreURL = `http://js.api.here.com/${_v}/mapsjs-core.js`;
+
+  return getJS(coreURL)
+    .then(function() {
+      return getJS(urls, {});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export default scriptLoader;
